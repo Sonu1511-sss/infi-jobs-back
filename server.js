4489f9bd33329bedf9e3d3c7a2a3c74596830568
shubham <shubhamuprade0@gmail.com>
2024-11-17 17:28:53 +0530

    first commit

--- server.js
+++ server.js
@@ -3,25 +3,39 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./Config/db');
 const authRoutes = require('./Routes/authRoutes');
-const errorHandler = require('./Middleware/errorHandler');
+const path = require('path');
 
+// Environment variables load karo
 dotenv.config();
+
+// MongoDB connect karo
 connectDB();
 
-const app = express(); 
+const app = express();
+const _dirname = path.resolve();
 
-// Middleware
+// Middleware use karo
 app.use(express.json());
-app.use(cors());
+app.use(cors()); // Allow karo Cross-Origin requests
 
-// Routes
+// Routes set karo
 app.use('/api/auth', authRoutes);
 
-// Error handling middleware should be added after the routes
-app.use(errorHandler);
+// app.use(cors({
+//     origin: ["https://infinity-jobs-main-2.vercel.app"],
+//     methods: ["POST", "GET"],
+//     credentials: true, // Fixed typo here
+// }));
 
-// server
+// Static files serve karo
+app.use(express.static(path.join(_dirname, 'client/dist')));
+
+// Client ki main index.html serve karo
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'dist', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server http://localhost:${PORT} par chal raha hai`));
 
 module.exports = app;
